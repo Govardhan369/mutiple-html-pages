00e29d57a89801aa74da400adeb625794d27d0f3
Govardhan369 <govardhanreddy88t@gmail.com>
2023-11-27 17:32:19 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Govardhan1(request):
+    return render(request,'Govardhan1.html')
+
+def Govardhan2(request):
+    return render(request,'Govardhan2.html')
+
+def Govardhan3(request):
+    return render(request,'Govardhan3.html')
+
+def Govardhan4(request):
+    return render(request,'Govardhan4.html')
